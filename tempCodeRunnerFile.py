275b18a6843d78561bd959e7844d4257ffbb7dac
NITHANR <nithanr39@gmail.com>
2024-09-04 16:40:47 +0530

    feat:pixelrunnerProject

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+game_name = test_font.render('Pixel Runner',False,(111,196,169))
+# game_name_rect = game_name.get_rect(center = (400,80))
+
+# game_message = test_font.render('Press space to run',False,(111,196,169))
+# game_
